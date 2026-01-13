ca3a30d7b0d27746206df4d25b4edda184ae4565
immiaaxovo <immiaaxovo@gmail.com>
2026-01-13 15:13:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ let clickCount = 0; // 记录点击 No 的次数
 
 // No 按钮的文字变化
 const noTexts = [
-  "？你认真的吗…",
-  "要不再想想？",
-  "不许选这个！ ",
-  "我会很伤心…",
-  "不行:(",
+  "你妈死了",
+  "我日你爸",
+  "选这个死全家 ",
+  "你变成了蛋小黄",
+  "你有病病叭",
 ];
 
 // No 按钮点击事件
@@ -57,7 +57,7 @@ noButton.addEventListener("click", function () {
 });
 
 // Yes 按钮点击后，进入表白成功页面
-const loveTest = `!!!窝也喜欢泥!! ( >᎑<)♡︎ᐝ  ${
+const loveTest = `!!!真好吃吖真好吃!! ( >᎑<)♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
 
@@ -77,3 +77,4 @@ yesButton.addEventListener("click", function () {
   document.body.style.overflow = "hidden";
 
 });
+
